8d728e37ac7db1f88f64939f37db9d0d3adb30ac
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-04-20 00:21:15 +0530

    Define functions to load and unload dictionary from memory

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,7 @@
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define FNV_OFFSET_BASIS_32 2166136261
 #define FNV_PRIME_32 16777619
@@ -46,22 +48,63 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
+    if (!file)
+    {
+        return false;
+    }
+
+    // Read strings from file one at a time
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        // Creating a new node for each word
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n->word, word);
+        n->next = NULL;
+
+        // Hash word to obtain a hash value
+        int hash_index = hash(word);
+
+        // Insert node into hash table at that location
+        if (table[hash_index] == NULL)
+        {
+            table[hash_index] = n;
+        }
+        else
+        {
+            n->next = table[hash_index];
+            table[hash_index] = n;
+        }
+        total_words++;
+    }
+    fclose(file);
+    return true;
 }
 
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return total_words;
 }
 
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < N; i++)
+    {
+        for (node *head = table[i]; head != NULL;)
+        {
+            node *cursor = head->next;
+            free(head);
+            head = cursor;
+        }
+    }
+    return true;
 }
